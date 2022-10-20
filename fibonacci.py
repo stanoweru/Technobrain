f44570dada943a7527a4b9f0b925a08913ad87d1
Stanley Waweru <stanoweru@gmail.com>
2022-10-20 10:48:34 +0300

    Fibo

---
+++ fibonacci.py
@@ -0,0 +1,23 @@
+# To display the Fibonacci
+
+Fiboterms = int(input("How many terms? "))
+
+# the ten terms
+fib1, fib2, fib3, fib4, fib5, fib6, fib7, fib8, fib9, fib10 = 0, 1,1,2,3,5,8,13,21,34
+count = 0
+
+if Fiboterms <= 0:
+   print("Please enter a positive integer")
+# if one return fib1
+elif Fiboterms == 1:
+   print("Fibonacci sequence upto",Fiboterms,":")
+   print(fib1)
+# generat fibo sequence
+else:
+   print("Fibonacci sequence:")
+   while count < Fiboterms:
+       print(fib1)
+       nth = fib1 + fib2
+       fib1 = fib2
+       fib2 = nth
+       count += 1
